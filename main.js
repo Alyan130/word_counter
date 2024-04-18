2ff1dd1890ff22a5b220744b728fc799c6aa6e2c
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-04-18 02:58:59 -0700

    words counter

---
+++ main.js
@@ -0,0 +1,10 @@
+import inquirer from "inquirer";
+const input = await inquirer.prompt([
+    {
+        message: "Enter your sentence:\n",
+        type: "String",
+        name: "sentence",
+    }
+]);
+const counter = input.sentence.trim().split(" ");
+console.log(`Words is your sentence are : ${counter.length}`);
